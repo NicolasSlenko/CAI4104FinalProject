b200cc797b93693e819440e71293f13870cc8150
CVMauro <maurochavezvega@ufl.edu>
2025-04-20 09:16:38 -0400

    created model architecture and training loop

---
+++ model_setup.py
@@ -0,0 +1,116 @@
+import ignite.engine
+import ignite.engine.events
+import ignite.metrics
+import torch
+import time
+import ignite
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import ignite.handlers as handlers
+
+from ignite.engine import create_supervised_trainer, create_supervised_evaluator, Events
+from torch.nn import Flatten, Linear, Conv2d, MaxPool2d, Dropout, Sequential, ReLU, BatchNorm2d
+
+def create_compile_model(channel=3, info=False):
+    class FPCNN(nn.Module):
+        def __init__(self, channel):
+            super(FPCNN, self).__init__()
+            self.conv1 = Conv2d(channel, 16, 3, padding='same')
+            self.batchnorm1 = BatchNorm2d(16)
+            self.pool1 = MaxPool2d(2)
+            self.conv2 = Conv2d(16, 32, 3, padding='same')
+            self.batchnorm2 = BatchNorm2d(32)
+            self.pool2 = MaxPool2d(4)
+            self.conv3 = Conv2d(32, 64, 3, padding='same')
+            self.batchnorm3 = BatchNorm2d(64)
+
+            self.fc1 = Linear(64 * 8 * 8, 1024)
+            self.dropout = Dropout(0.4)
+            self.fc2 = Linear(1024, 256)
+            self.fc3 = Linear(256, 12)
+        def forward(self, x):
+            x = self.pool1(F.relu(self.batchnorm1(self.conv1(x))))
+            x = self.pool1(F.relu(self.batchnorm2(self.conv2(x))))
+            x = self.pool2(F.relu(self.batchnorm3(self.conv3(x))))
+            #print(x.shape)
+            
+            x = x.view(-1, 64 * 8 * 8)
+            x = F.relu(self.fc1(x))
+            x = self.dropout(x)
+            x = F.relu(self.fc2(x))
+            x = self.dropout(x)
+            #x = self.dropout(x)
+            x = self.fc3(x)
+            #print(x.shape)
+
+            return x
+    
+    model = FPCNN(channel)
+    print(model)
+
+    optimizer = optim.Adam(model.parameters(), lr=0.0004, weight_decay=0.00002)
+    model.optimizer = optimizer
+    model.loss_func = nn.CrossEntropyLoss()
+
+    return model
+
+def train_model(device, train_loader, val_loader, max_epochs, patience=3):
+    # create and compile model and then move to GPU
+    model = create_compile_model()
+    model.to(device)
+    print(device)
+
+    loss_func = model.loss_func
+    optimizer = model.optimizer
+
+    start = time.time()
+    for epoch in range(max_epochs):
+        model.train()
+
+        train_loss = 0.0
+        correct_train_num = 0
+        total_train_size = 0
+        for inputs, labels in train_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            #print(labels[:10])
+            optimizer.zero_grad()
+            
+            outputs = model(inputs)
+            loss = loss_func(outputs, labels)
+            
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item() * inputs.size(0)
+            predictions = torch.argmax(outputs, 1)
+            correct_train_num += (predictions == labels).sum().item()
+            total_train_size += labels.size(0)
+        train_loss /= len(train_loader.dataset)
+        train_acc = correct_train_num / total_train_size
+
+        model.eval()
+        val_loss = 0
+        correct_val_num = 0
+        total_val_size = 0
+        with torch.no_grad():
+            for inputs, labels in val_loader:
+                inputs, labels = inputs.to(device), labels.to(device)
+                
+                outputs = model(inputs)
+                loss = loss_func(outputs, labels)
+                val_loss += loss.item() * inputs.size(0)
+
+                predictions = torch.argmax(outputs, 1)
+                correct_val_num += (predictions == labels).sum().item()
+                total_val_size += labels.size(0)
+        val_loss /= len(val_loader.dataset)
+        val_acc = correct_val_num / total_val_size
+    
+        print(f'Epoch: {epoch+1} | Training Loss: {train_loss:.4f}, Training Accuracy: {100*train_acc:.2f}% | Validation Loss: {val_loss:.4f} Validation Accuracy: {100*val_acc:.2f}%')
+    end = time.time()
+
+    elapsed_training_time = end-start
+    print(elapsed_training_time)
+    
