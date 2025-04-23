22c7e4e2928915d230389ac0db15c2ac560ef587
Khang Tran <116851477+NPKLTacocat@users.noreply.github.com>
2025-04-22 20:11:19 -0400

    Update model architecture by removing dropout and adding more layers to make the architecture more funnel like

--- model_setup.py
+++ model_setup.py
@@ -14,55 +14,76 @@ import numpy as np
 
 from ignite.engine import create_supervised_trainer, create_supervised_evaluator, Events
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-from torch.nn import Flatten, Linear, Conv2d, MaxPool2d, Dropout, Sequential, ReLU, BatchNorm2d, Dropout2d
+from torch.nn import (
+    Flatten,
+    Linear,
+    Conv2d,
+    MaxPool2d,
+    Dropout,
+    Sequential,
+    ReLU,
+    BatchNorm2d,
+    Dropout2d,
+)
+
 
 def create_compile_model(channel=3, info=False):
     class FPCNN(nn.Module):
         def __init__(self, channel):
             super(FPCNN, self).__init__()
-            self.conv1 = Conv2d(channel, 16, 3, stride=2)
-            #self.conv1b = Conv2d(16, 16, 3, padding='same')
+            self.conv1 = Conv2d(channel, 16, 3, padding="same")
             self.batchnorm1 = BatchNorm2d(16)
-            self.pool1 = MaxPool2d(2)
-            self.dropout2d = Dropout2d(0.4)
-            self.conv2 = Conv2d(16, 32, 3)
-            #self.adaptpool = nn.AdaptiveAvgPool2d(8)
+            self.pool = MaxPool2d(2)
+
+            self.conv2 = Conv2d(16, 32, 3, padding="same")
             self.batchnorm2 = BatchNorm2d(32)
-            self.conv3 = Conv2d(32, 64, 3)
+
+            self.conv3 = Conv2d(32, 64, 3, padding="same")
             self.batchnorm3 = BatchNorm2d(64)
 
-            self.fc1 = Linear(64 * 6 * 6, 512)
-            self.dropout = Dropout(0.4)
-            #self.fc2 = Linear(512, 128)
-            self.fc2 = Linear(512, 12)
+            self.conv4 = Conv2d(64, 128, 3, padding="same")
+            self.batchnorm4 = BatchNorm2d(128)
+
+            self.fc1 = Linear(128 * 8 * 8, 1024)
+            self.dropout1 = Dropout(0.5)
+            self.fc2 = Linear(1024, 512)
+            self.dropout2 = Dropout(0.5)
+            self.fc3 = Linear(512, 256)
+            self.dropout3 = Dropout(0.5)
+            self.fc4 = Linear(256, 12)
+
         def forward(self, x):
-            x = self.pool1(self.dropout2d(self.batchnorm1(F.relu(self.conv1(x)))))
-            x = self.pool1(self.dropout2d(self.batchnorm2(F.relu(self.conv2(x)))))
-            x = self.pool1(self.dropout2d(self.batchnorm3(F.relu(self.conv3(x)))))
-            #x = F.relu(self.batchnorm1(self.conv1(x)))
-            #x = self.pool1(F.relu(self.batchnorm1(self.conv1b(x))))
-            #x = self.pool1(F.relu(self.batchnorm1(self.conv1(x))))
-            #print(x.shape)
-            
-            x = x.view(-1, 64 * 6 * 6)
+            x = self.pool(F.relu(self.batchnorm1(self.conv1(x))))
+            x = self.pool(F.relu(self.batchnorm2(self.conv2(x))))
+            x = self.pool(F.relu(self.batchnorm3(self.conv3(x))))
+            x = self.pool(F.relu(self.batchnorm4(self.conv4(x))))
+            # print(x.shape)
+
+            x = x.view(-1, 128 * 8 * 8)
+
             x = F.relu(self.fc1(x))
-            x = self.dropout(x)
-            #x = F.relu(self.fc2(x))
-            #x = self.dropout(x)
-            #x = self.dropout(x)
-            x = self.fc2(x)
-            #print(x.shape)
+            x = self.dropout1(x)
+            x = F.relu(self.fc2(x))
+            x = self.dropout2(x)
+            x = F.relu(self.fc3(x))
+            x = self.dropout3(x)
+            x = self.fc4(x)
+            # print(x.shape)
 
             return x
-    
+            # print(x.shape)
+
+            return x
+
     model = FPCNN(channel)
     print(model)
 
-    optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
+    optimizer = optim.Adam(model.parameters(), lr=0.0005, weight_decay=0.0005)
     model.optimizer = optimizer
     model.loss_func = nn.CrossEntropyLoss()
 
     return model
+
 
 def train_model(device, train_loader, val_loader, dataset, max_epochs, patience=10):
     # create and compile model and then move to GPU
@@ -73,14 +94,13 @@ def train_model(device, train_loader, val_loader, dataset, max_epochs, patience=
     loss_func = model.loss_func
     optimizer = model.optimizer
 
-    best_val_loss = float('inf')
+    best_val_loss = float("inf")
 
-    scheduler = ReduceLROnPlateau(optimizer, 'max', factor=0.5, patience=4)
+    scheduler = ReduceLROnPlateau(optimizer, "max", factor=0.5, patience=4)
 
     os.makedirs("checkpoints", exist_ok=True)
     early_stop_path = "checkpoints/early_stop_model.pth"
     best_model_state = None
-    
 
     start = time.time()
     for epoch in range(max_epochs):
@@ -91,12 +111,12 @@ def train_model(device, train_loader, val_loader, dataset, max_epochs, patience=
         total_train_size = 0
         for inputs, labels in train_loader:
             inputs, labels = inputs.to(device), labels.to(device)
-            #print(labels[:10])
+            # print(labels[:10])
             optimizer.zero_grad()
-            
+
             outputs = model(inputs)
             loss = loss_func(outputs, labels)
-            
+
             loss.backward()
             optimizer.step()
 
@@ -114,7 +134,7 @@ def train_model(device, train_loader, val_loader, dataset, max_epochs, patience=
         with torch.no_grad():
             for inputs, labels in val_loader:
                 inputs, labels = inputs.to(device), labels.to(device)
-                
+
                 outputs = model(inputs)
                 loss = loss_func(outputs, labels)
                 val_loss += loss.item() * inputs.size(0)
@@ -127,8 +147,10 @@ def train_model(device, train_loader, val_loader, dataset, max_epochs, patience=
 
         scheduler.step(val_acc)
         print(scheduler.get_last_lr())
-    
-        print(f'Epoch: {epoch+1} | Training Loss: {train_loss:.4f}, Training Accuracy: {100*train_acc:.2f}% | Validation Loss: {val_loss:.4f} Validation Accuracy: {100*val_acc:.2f}%')
+
+        print(
+            f"Epoch: {epoch+1} | Training Loss: {train_loss:.4f}, Training Accuracy: {100*train_acc:.2f}% | Validation Loss: {val_loss:.4f} Validation Accuracy: {100*val_acc:.2f}%"
+        )
         if val_loss < best_val_loss:
             patience_count = 0
             best_val_loss = val_loss
@@ -138,13 +160,14 @@ def train_model(device, train_loader, val_loader, dataset, max_epochs, patience=
             print("worse")
             if patience <= patience_count:
                 print(f"Early stopping on epoch {epoch+1}")
-                torch.save(model.state_dict(), early_stop_path) # save early stop checkpoint                                                                                       
+                torch.save(
+                    model.state_dict(), early_stop_path
+                )  # save early stop checkpoint
                 break
     end = time.time()
 
-    elapsed_training_time = end-start
-    print(f'Training and Validation time: {elapsed_training_time}')
+    elapsed_training_time = end - start
+    print(f"Training and Validation time: {elapsed_training_time}")
     # Save the model after training is complete
     model_path = "checkpoints/final_model.pth"
     torch.save(model.state_dict(), model_path)
-    
--- model_train.py
+++ model_train.py
@@ -4,13 +4,15 @@ from model_setup import train_model
 
 from data_processing import preprocessing, visualize_class_distribution
 from matplotlib import pyplot as plt
-plt.rcParams.update({'font.size': 14})
+
+plt.rcParams.update({"font.size": 14})
+
 
 def main():
     train_loader, val_loader, test_loader, full_dataset = preprocessing()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    #visualize_class_distribution(full_dataset)
-    train_model(device, train_loader, val_loader, full_dataset, max_epochs=25)
+    # visualize_class_distribution(full_dataset)
+    train_model(device, train_loader, val_loader, full_dataset, max_epochs=100)
 
 
 if __name__ == "__main__":
